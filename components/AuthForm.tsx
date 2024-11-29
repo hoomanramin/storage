328dc78e819e8c3e834ada0fd5085cef045e820c
Hooman Ramin <hoomanramin@gmail.com>
2024-11-29 09:49:14 +0330

    bulding the auth with react hook form and shadcn and zod in mix

---
+++ components/AuthForm.tsx
@@ -0,0 +1,133 @@
+'use client';
+
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type AuthFormProps = 'sign-in' | 'sign-up';
+const authFormSchema = (formType: AuthFormProps) => {
+  return z.object({
+    email: z.string().email(),
+    fullname:
+      formType === 'sign-up'
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
+
+const AuthForm = ({ type }: { type: AuthFormProps }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullname: '',
+      email: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+        <h1 className="form-title">
+          {type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+        </h1>
+        {type === 'sign-up' && (
+          <FormField
+            control={form.control}
+            name="fullname"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Full Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="shad-input"
+                      placeholder="Enter your full name"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+        )}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <div className="shad-form-item">
+                <FormLabel className="shad-form-label">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    className="shad-input"
+                    placeholder="Enter your email"
+                    {...field}
+                  />
+                </FormControl>
+              </div>
+
+              <FormMessage className="shad-form-message" />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type="submit"
+          className="form-submit-button"
+          disabled={isLoading}
+        >
+          {type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+          {isLoading && (
+            <Image
+              src="/assets/icons/loader.svg"
+              alt="loading"
+              width={24}
+              height={24}
+              className="ml-2 animate-spin"
+            />
+          )}
+        </Button>
+        {errorMessage && <div className="error-message">*{errorMessage}</div>}
+        <div className="body-2 flex justify-center">
+          <p className="text-light-100">
+            {type === 'sign-in'
+              ? "You don't have an account? "
+              : 'Already have an account? '}
+          </p>
+          <Link
+            className="ml-1 font-medium text-brand"
+            href={type === 'sign-in' ? '/sign-up' : '/sign-in'}
+          >
+            {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+          </Link>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default AuthForm;
