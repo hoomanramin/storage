d5326a9302fb1619322ec073f9e82134fdf3fc62
Hooman Ramin <hoomanramin@gmail.com>
2024-12-03 18:12:39 +0330

    remove Home component from the main page

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="center">
-      <h1 className="text-3xl text-brand">
-        The only thing that is missing is the Storage
-      </h1>
-    </div>
-  );
-}
